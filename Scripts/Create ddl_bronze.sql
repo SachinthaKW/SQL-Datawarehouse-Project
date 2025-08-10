776f657c37f878ee4e507bd34792382e1b702ffc
Sachintha KahaweWithana <108917836+SachinthaKW@users.noreply.github.com>
2025-08-11 06:15:43 +1200

    Update Create ddl_bronze.sql

--- Scripts/Create ddl_bronze.sql
+++ Scripts/Create ddl_bronze.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE bronze.crm_cust_info;
@@ -78,3 +88,4 @@ CREATE TABLE bronze.erp_PX_CAT_G1V2 (
 	MAINTENANCE NVARCHAR(50) 
 );
 GO
+

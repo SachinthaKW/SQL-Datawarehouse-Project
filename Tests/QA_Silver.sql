260ee6e6657a752ebc0e53d37f755f7342520888
Sachintha KahaweWithana <108917836+SachinthaKW@users.noreply.github.com>
2025-08-19 11:05:59 +1200

    Update QA_Silver.sql

--- Tests/QA_Silver.sql
+++ Tests/QA_Silver.sql
@@ -12,7 +12,7 @@ Script Purpose:
     - Data consistency between related fields.
 
 Usage Notes:
-    - Run these checks after data loading Silver Layer.
+    - Run these checks after loading data to the Silver Layer.
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
@@ -30,5 +30,107 @@ GROUP BY cst_id
 HAVING COUNT(*) >1 
 OR cst_id IS NULL
 
+-- Check for unwanted spaces in First Name, Last Name & Gender
+-- Expectation: No Result
+
+SELECT cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname)
+
+SELECT cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname)
+
+SELECT cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr)
+
+-- Data standardization & Consistency in Gender & Marital Status Columns   
+
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+SELECT DISTINCT cst_marital_status
+FROM bronze.crm_cust_info
+
+-- ====================================================================
+-- Checking 'silver.crm_prd_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- ====================================================================
+-- Checking 'silver.crm_sales_details'
+-- ====================================================================
+-- Check for Invalid Dates
+-- Expectation: No Invalid Dates
+
+
+
+
+
+
+
+
+
+
+-- ====================================================================
+-- Checking 'silver.erp_cust_az12'
+-- ====================================================================
+-- Identify Out-of-Range Dates
+-- Expectation: Birthdates between 1924-01-01 and Today
+
+
+
+
+
+
+-- ====================================================================
+-- Checking 'silver.erp_loc_a101'
+-- ====================================================================
+-- Data Standardization & Consistency
+
+
+
+
+
+
+
+
+
+
+-- ====================================================================
+-- Checking 'silver.erp_px_cat_g1v2'
+-- ====================================================================
 -- Check for Unwanted Spaces
 -- Expectation: No Results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

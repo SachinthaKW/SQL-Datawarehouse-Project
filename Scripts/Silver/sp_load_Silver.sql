7e34bba61fca74b9f3af88cec03626b2c4b5a586
Sachintha KahaweWithana <108917836+SachinthaKW@users.noreply.github.com>
2025-08-19 11:58:59 +1200

    Update and rename QA_Silver.sql to sp_load_Silver.sql

---
+++ Scripts/Silver/sp_load_Silver.sql
@@ -0,0 +1,62 @@
+/*
+===============================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+	Actions Performed:
+		- Truncates Silver tables.
+		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		
+Parameters:
+    None. 
+	  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC Silver.load_silver;
+===============================================================================
+*/
+
+CREATE OR 
+PRINT '>> Truncating Table: silver.crm_cust_info';
+
+TRUNCATE TABLE silver.crm_cust_info;
+
+PRINT '>> Inserting Data into: silver.crm_cust_info';
+
+INSERT INTO silver.crm_cust_info (
+	cst_id,
+	cst_key,
+	cst_firstname,
+	cst_lastname,
+	cst_marital_status,
+	cst_gndr,
+	cst_create_date
+)
+
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+CASE WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+	 WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+	 ELSE 'N/A'
+END cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+	 ELSE 'N/A'
+END cst_gndr,	 
+cst_create_date
+FROM(
+	SELECT 
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info
+	WHERE cst_id IS NOT NULL
+)t WHERE flag_last = 1
+
+SELECT 
+*
+FROM silver.crm_cust_info

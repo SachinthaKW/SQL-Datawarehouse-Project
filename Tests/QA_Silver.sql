1112d5104b1f1ef4f77d57624acf28e43cf91dfb
Sachintha KahaweWithana <108917836+SachinthaKW@users.noreply.github.com>
2025-08-20 03:31:10 +1200

    Update QA_Silver.sql

--- Tests/QA_Silver.sql
+++ Tests/QA_Silver.sql
@@ -103,12 +103,57 @@ WHERE prd_end_dt < prd_start_dt;
 
 
 
+-- Check for invalid dates
+
+SELECT 
+	sls_due_dt
+FROM silver.crm_sales_details
+WHERE sls_due_dt <= 0
+OR LEN(sls_due_dt) !=8
+OR sls_due_dt > 20500101
+OR sls_due_dt < 19000101
+
+SELECT 
+	NULLIF(sls_order_dt,0) sls_order_dt
+FROM silver.crm_sales_details
+WHERE sls_order_dt <= 0
+OR LEN(sls_order_dt) !=8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
 
 
+SELECT 
+	NULLIF(sls_ship_dt,0) sls_ship_dt
+FROM silver.crm_sales_details
+WHERE sls_ship_dt <= 0
+OR LEN(sls_ship_dt) !=8
+OR sls_ship_dt > 20500101
+OR sls_ship_dt < 19000101
 
+-- Check for Invalid Date Orders (Order Date > Shipping/Due Dates)
+-- Expectation: No Results
+	
+SELECT
+*
+FROM silver.crm_sales_details
+WHERE sls_ship_dt < sls_order_dt
 
+-- Check Data Consistency: Between Sales, Quantity, and Price
+-- >> Sales = Quantity * Price
+-- >> Values must not be NULL, zero or Negative
 
+SELECT DISTINCT
+	sls_sales ,
+	sls_quantity,
+	sls_price
+FROM silver.crm_sales_details
+WHERE sls_sales != sls_quantity * sls_price
+	  OR sls_sales IS NULL OR sls_price IS NULL OR sls_quantity IS NULL
+	  OR sls_sales <=0 OR sls_price <=0 OR sls_quantity <=0
+ORDER BY sls_sales,sls_quantity,sls_price
 
+SELECT *
+FROM silver.crm_sales_details
 
 -- ====================================================================
 -- Checking 'silver.erp_cust_az12'
